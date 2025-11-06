fe6a71e38b1f74573134328b629b482d7d67163f
JSavill03 <joshua.savill@nhs.net>
2025-11-06 15:52:43 +0000

    End of The Course

---
+++ SQLLesson.sql
@@ -0,0 +1,27 @@
+
+-- SELECT 
+--     ps.PatientId
+--     , ps.Hospital
+--     , ps.Ward
+--     , ps.AdmittedDate 
+--     , ps.DischargeDate
+--     , ps.Tariff
+--     , DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+--     , DATEDIFF(DAY, ps.AdmittedDate , ps.DischargeDate)+1 AS DaysInHospital
+-- FROM PatientStay AS ps
+
+-- WHERE ps.Hospital IN ('KINGSTON','PRUH')
+-- ORDER BY 
+--     ps.PatientID DESC
+
+SELECT 
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,h.HospitalType
+    ,h.Hospital 
+    ,ps.Hospital    
+FROM PatientStay ps 
+left JOIN DimHospitalBad h 
+    ON ps.Hospital = h.Hospital
+
+
